d46013e6130dd66203360364062d38037f9bc25d
hht <hht@163.com>
2020-09-29 11:54:56 +0800

    feat: add third party libraries

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,8 @@ module.exports = {
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  rules: {
+    'comma-dangle': ['error', 'never'],
+    semi: ['error', 'never']
+  }
 };
